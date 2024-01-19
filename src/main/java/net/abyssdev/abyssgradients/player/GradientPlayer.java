77e1f02705061ab7365a33920ffb331b1c168924
RelocationDev <g.delgado2006@gmail.com>
2024-01-19 17:55:28 -0500

    update config and project ver

--- src/main/java/net/abyssdev/abyssgradients/player/GradientPlayer.java
+++ src/main/java/net/abyssdev/abyssgradients/player/GradientPlayer.java
@@ -1,18 +1,14 @@
 package net.abyssdev.abyssgradients.player;
 
-import dev.morphia.annotations.Converters;
-import dev.morphia.converters.UUIDConverter;
 import lombok.Data;
 import net.abyssdev.abysslib.storage.id.Id;
 
 import java.util.UUID;
 
 @Data
-@Converters(UUIDConverter.class)
 public final class GradientPlayer {
 
     @Id
-    @dev.morphia.annotations.Id
     private final UUID uuid;
 
     private String gradient;
